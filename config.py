7324b7d96890926395573c97fd83c25c0ac58db9
kartiknishad9579 <kartiknishad9579@gmail.com>
2026-07-20 14:51:08 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+API_ID = 36407155
+API_HASH = "fb803f15ead1178e63897d77148b7bef" # @mytelegram.org se le
+BOT_TOKEN = "BOT_TOKEN_YAHA_DAALO" # @BotFather
+STRING_SESSION = "STRING_SESSION_YAHA_DAALO" # @StringSessionBot
+OWNER_ID = 8892716197 # tera telegram id
+SUPPORT_LINK = "https://t.me" # apna support group
+CHANNEL_LINK = "https://t.me" # apna channel
