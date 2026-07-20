63c4cf59c482b669e62d8caafbfc9fd8782daf69
kartiknishad9579 <kartiknishad9579@gmail.com>
2026-07-21 00:08:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,9 @@
 import os
-API_ID = int(os.environ.get("API_ID"))
+
+API_ID = int(os.environ.get("API_ID", 36407155))
 API_HASH = os.environ.get("API_HASH")
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
-OWNER_ID = int(os.environ.get("OWNER_ID"))
-SUPPORT_LINK = os.environ.get("SUPPORT_LINK", "https://t.me")
-CHANNEL_LINK = os.environ.get("CHANNEL_LINK", "https://t.me")
+OWNER_ID = int(os.environ.get("OWNER_ID", 8892716197))
+LOG_GROUP = int(os.environ.get("LOG_GROUP", -1002309569603)) 
+SUPPORT_LINK = os.environ.get("SUPPORT_LINK", "https://t.me/jesa_ho_wesa_hi_ho")
+CHANNEL_LINK = os.environ.get("CHANNEL_LINK", "https://t.me/ye_duniya_ek_sapna_he")
