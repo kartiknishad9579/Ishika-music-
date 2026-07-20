ecaf421a4c57dfca91b5ce2ddfc1e5627cd877aa
kartiknishad9579 <kartiknishad9579@gmail.com>
2026-07-20 14:54:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-API_ID = 36407155
-API_HASH = "fb803f15ead1178e63897d77148b7bef" # @mytelegram.org se le
-BOT_TOKEN = "BOT_TOKEN_YAHA_DAALO" # @BotFather
-STRING_SESSION = "STRING_SESSION_YAHA_DAALO" # @StringSessionBot
-OWNER_ID = 8892716197 # tera telegram id
-SUPPORT_LINK = "https://t.me" # apna support group
-CHANNEL_LINK = "https://t.me" # apna channel
+import os
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+OWNER_ID = int(os.environ.get("OWNER_ID"))
+SUPPORT_LINK = os.environ.get("SUPPORT_LINK", "https://t.me")
+CHANNEL_LINK = os.environ.get("CHANNEL_LINK", "https://t.me")
